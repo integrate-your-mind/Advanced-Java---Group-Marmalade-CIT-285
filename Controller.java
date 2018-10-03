f4ad8c1a9d88d1a8a492f41668fbe5b3936b6fd7
integrate-your-mind <43152353+integrate-your-mind@users.noreply.github.com>
2018-10-03 14:24:05 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,5 +1,16 @@
 package sample;
 
 public class Controller {
+    //Roman Mondello 
+    //Login
+    //Identify if a "Doctor" or a "Patient"
+    //If "Doctor" get doctor information from DB
+    //If "Patient" get patient information from DB
 
+    //User interacts...
+
+    //On exit save user information and upload back to DB
+    //End
+    //Roman Mondello 
 }
+
