daae981cb2bb3f7160eda4f2f693acf4b7dc6f70
integrate-your-mind <43152353+integrate-your-mind@users.noreply.github.com>
2018-10-03 14:27:19 -0400

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,15 +1,14 @@
-/*
- * Copyright (c) Roman Mondello
- */
-
 package sample;
 
 import java.util.Map;
 
 public class Doctor extends Provider {
-
+    
+    //Roman Mondello
     private Patient currentPatient = new Patient();
-
+    //Roman Mondello
+    
+    //Roman Mondello
     public void viewPatientRecord(Map<Account, Account> accounts, Account account){
         //Code goes here for viewing the patient record via a hashcode lookup
         //from the controller
@@ -19,4 +18,5 @@ public class Doctor extends Provider {
             //Such as Diagnosis, Appointments, and Test Results
         }
     }
+    //Roman Mondello
 }
